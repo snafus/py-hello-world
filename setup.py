37cb71fb064cbfc0a971b35e62bc23098213bd06
James <james.walder@gmail.com>
2016-01-23 17:20:21 +0000

    Setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='py-hello-world',
+      version='0.1',
+      description='Sample hello world app'
+      author='',
+      author_email='',
+      #url='https://www.python.org/sigs/distutils-sig/',
+      packages=['py-hello-world'],
+     )
+
+
